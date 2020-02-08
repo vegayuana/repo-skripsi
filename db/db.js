2c8d4d18ff8211e08d0baf8c5294e4f6e72e3dda
vega <vegayuana@gmail.com>
2020-02-08 12:35:04 +0700

    deploy

--- db/db.js
+++ db/db.js
@@ -1,11 +1,9 @@
 const mysql = require('mysql') 
-const db = mysql.createPool(
-  // host: process.env.DB_HOST,
-  // user: process.env.DB_USER,
-  // password: process.env.DB_PASS,
-  // database: process.env.MYSQL_DB,
-  // timezone: 'UTC+0',
-  // uri:'mysql://b603329b79d710:1c769385@us-cdbr-iron-east-04.cleardb.net/heroku_0d41974a8e076cc?reconnect=true'
-  process.env.CLEARDB_DATABASE_URL
-)
+const db = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.MYSQL_DB,
+  timezone: 'UTC+0'
+})
 module.exports = db
