334755c8e82c210bed4c5069a5d340636ad274a8
Muhamad Yusrizan <gmelclod@gmail.com>
2020-02-08 20:35:37 +0700

    change db

--- db/db.js
+++ db/db.js
@@ -1,9 +1,9 @@
-const mysql = require('mysql') 
+const mysql = require("mysql");
 const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.MYSQL_DB,
-  timezone: 'UTC+0'
-})
-module.exports = db
+  host: "us-cdbr-iron-east-04.cleardb.net",
+  user: "b603329b79d710",
+  password: "1c769385",
+  database: "heroku_0d41974a8e076cc",
+  timezone: "UTC+0"
+});
+module.exports = db;
