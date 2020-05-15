d26e2f2c34239cbd559741a07537566e7f62de79
vega <vegayuana@gmail.com>
2020-05-15 11:18:32 +0700

    test

--- client/src/serviceWorker.js
+++ client/src/serviceWorker.js
@@ -20,8 +20,7 @@ export function register(config) {
 
     //Ketika halaman telah di load 
     window.addEventListener('load', () => {
-      // const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`
-      const swUrl = `/service-worker.js`
+      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`
       if (isLocalhost) {
         checkValidServiceWorker(swUrl, config)
         navigator.serviceWorker.ready.then(() => {
